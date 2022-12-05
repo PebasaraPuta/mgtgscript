11edf8c4e7e2f272f4333a5a2c8518b7f2500189
Prabhasha :) <117509567+PebasaraPuta@users.noreply.github.com>
2022-12-04 17:04:03 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://pebasaraputa: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@github.com/pebasaraputa/sindupothabot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://pebasaraputa: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@github.com/pebasaraputa/sindupothabot okkk && cd okkk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
